66967ad8987397e904e4f760e1ef5e6e763abc32
Shota-Sunada <shota.sunada.dev@gmail.com>
2026-01-26 16:27:59 +0900

    エラー修正

--- Submerged/BaseGame/Patches/TextTranslatorTMP.cs
+++ Submerged/BaseGame/Patches/TextTranslatorTMP.cs
@@ -1,4 +1,4 @@
-using HarmonyLib;
+﻿using HarmonyLib;
 using TMPro;
 
 namespace Submerged.BaseGame.Patches;
@@ -8,15 +8,19 @@ public static class TextTranslatorTMPPatches
 {
     [HarmonyPatch(typeof(TextTranslatorTMP), nameof(TextTranslatorTMP.ResetText))]
     [HarmonyPrefix]
-    [BaseGameCode(LastChecked.v17_0_0, "We are patching this with its own code to get rid of inlining that ruins our translation patches")]
+    [BaseGameCode(LastChecked.v17_1_0, "We are patching this with its own code to get rid of inlining that ruins our translation patches")]
     public static void ResetTextPatch(TextTranslatorTMP __instance, out bool __runOriginal)
     {
         __runOriginal = false;
 
-        if (__instance.ResetOnlyWhenNoDefault && string.IsNullOrEmpty(__instance.defaultStr)) return;
+        if (__instance == null || (__instance.ResetOnlyWhenNoDefault && string.IsNullOrEmpty(__instance.defaultStr))) return;
+
+        if (TranslationController.Instance == null) return;
 
         TextMeshPro component = __instance.GetComponent<TextMeshPro>();
         string text = TranslationController.Instance.GetStringWithDefault(__instance.TargetText, __instance.defaultStr);
+
+        if (text == null) return;
 
         if (__instance.ToUpper)
         {
@@ -31,8 +35,11 @@ public static class TextTranslatorTMPPatches
         else
         {
             TextMeshProUGUI component2 = __instance.GetComponent<TextMeshProUGUI>();
-            component2.text = text;
-            component2.ForceMeshUpdate();
+            if (component2 != null)
+            {
+                component2.text = text;
+                component2.ForceMeshUpdate();
+            }
         }
 
         __instance.OnTranslate?.Invoke();
